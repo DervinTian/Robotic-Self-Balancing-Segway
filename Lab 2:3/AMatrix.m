c102c9fd977ad966962432440056bd1cf4c43eb0
DervinTian <144625526+DervinTian@users.noreply.github.com>
2024-12-20 19:04:55 +0800

    Delete Lab 2:3 directory

--- Lab 2:3/AMatrix.m
+++
@@ -1,8 +0,0 @@
-function A = AMatrix()
-
-    global M g l J r m I 
-    
-  [0,                                                                                      0, 1, 0]
-[0,                                                                                      0, 0, 1]
-[0, -(M*g*l*(J + M*r^2 + m*r^2 + M*l*r))/(I*J + J*M*l^2 + I*M*r^2 + I*m*r^2 + M*l^2*m*r^2), 0, 0]
-[0,          (M*g*l*(J + M*r^2 + m*r^2))/(I*J + J*M*l^2 + I*M*r^2 + I*m*r^2 + M*l^2*m*r^2), 0, 0]
